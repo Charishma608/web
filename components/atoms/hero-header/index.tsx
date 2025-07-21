ec731fa279bb292e7bbd1c608966d8052c11db2f
Charishma Bollineni <139740665+Charishma608@users.noreply.github.com>
2025-07-21 19:46:37 +0530

    Update index.tsx

--- components/atoms/hero-header/index.tsx
+++ components/atoms/hero-header/index.tsx
@@ -3,35 +3,59 @@ import { useStyletron } from 'baseui';
 
 function HeroHeader() {
   const [css, theme] = useStyletron();
-  const dotStyle = { fontFamily: '"Arial", sans-serif' };
+  const dotStyle = { 
+    fontFamily: '"Arial", sans-serif',
+    color: '#00C29F' 
+  };
+  
   return (
-    <h1 className={css({
-      marginTop: 0,
-      marginBottom: '90px',
-      fontSize: '56px',
-      marginLeft:'-100px',
-      lineHeight: 1.2,
-      fontWeight: 700,
-      opacity: 1,
-      transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
-      transformStyle: 'preserve-3d',
-      [theme.mediaQuery.small]: { fontSize: '54px' },
-      [theme.mediaQuery.medium]: { fontSize: '76px' },
-      [theme.mediaQuery.large]: { fontSize: '50px' },
-    })}
-    >
-      Smart IT
-      <span style={dotStyle}>
-        .
-      </span>
-      <br />
-      Reliable Service
-      <span style={dotStyle}>.</span>
-      <br />
-      Always on Support
-      <span style={dotStyle}>.</span>
-      <br />
-    </h1>
+    <div className={css({
+      marginBottom: '40px',
+      [theme.mediaQuery.medium]: {
+        marginBottom: '60px',
+      },
+      [theme.mediaQuery.large]: {
+        marginBottom: '90px',
+      },
+    })}>
+      <h1 className={css({
+        marginTop: '40px',
+        marginBottom: '90px',
+        fontSize: '56px',
+        marginLeft: '-100px',
+        lineHeight: 1.2,
+        fontWeight: 700,
+        opacity: 1,
+        transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
+        transformStyle: 'preserve-3d',
+        whiteSpace: 'nowrap',
+        [theme.mediaQuery.small]: { 
+          fontSize: '32px',
+          marginBottom: '40px',
+          textAlign: 'center',
+          marginLeft: 0,
+          whiteSpace: 'normal',
+          marginTop: '20px',
+        },
+        [theme.mediaQuery.medium]: { 
+          fontSize: '40px',
+          textAlign: 'left',
+          marginLeft: 0,
+          whiteSpace: 'nowrap',
+          marginTop: '30px',
+        },
+        [theme.mediaQuery.large]: { 
+          fontSize: '56px',
+          marginLeft: '-100px',
+          marginTop: '40px',
+          whiteSpace: 'nowrap',
+        },
+      })}>
+        <div>Smart IT<span style={dotStyle}>.</span></div>
+        <div>Reliable Service<span style={dotStyle}>.</span></div>
+        <div>Always on Support<span style={dotStyle}>.</span></div>
+      </h1>
+    </div>
   );
 }
 
